242271cff3c69a4f90a15525987c539d3f24433a
basilaslam <basilaslamnp@gmail.com>
2023-09-20 17:06:39 +0530

    navbar almost done[static]

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
